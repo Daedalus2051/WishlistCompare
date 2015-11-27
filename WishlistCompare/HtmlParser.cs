43b997bd95ad71a5f8e824a432da32a04a0749d8
Russell Hoyer <sixstringtrebleterror@gmail.com>
2015-11-27 14:48:22 -0500

    Modified some of the code for GetWishlistGameData so that I could also get the steam ID of the game; this is necessary for the price history lookup Added GetLowestPrices method which takes a string steamGameID: this method works but should be cleaned up because I ran into an issue where the website started refusing to return data because "too many requests". This will work in small batches (tested 6 in one go, fine... tested 191... all went to hell).
    Notes:
    There are several schools of thought to the above issue. I could process in batches of n; or only look up the price history for those games on sale; or separate it out entirely so that I only load the game data and let the user query for the price history on the individual result.
    
    Also need to write the async calls for this method as it locks everything up (as expected)
    And I want to also look into implementing some event changed code to update the user on the progess
    
    Superficially, eventually want to add some graphical features to visibly show what games are on sale and have peak price points in the DataGrid...etc.

--- WishlistCompare/HtmlParser.cs
+++ WishlistCompare/HtmlParser.cs
@@ -29,18 +29,24 @@ namespace WishlistCompare
             WebClient client = new WebClient();
             string htmlCode = client.DownloadString(url); // get the raw HTML page returned from the URL call
             string gameName, gameRank, originalPrice = "$0.00", salePrice = "$0.00", salePercent = "-0", lowestRegPrice, lowestSalePrice;
+            string gameID;
 
             HtmlDocument hdoc = new HtmlDocument();
             hdoc.LoadHtml(htmlCode);
 
-            // Grab the wishlistRowItem tags, this will indicate each game that is in the list
-            foreach (HtmlNode wishlistRowItem in hdoc.DocumentNode.SelectNodes("//div[@class='wishlistRowItem']"))
+            // Grab the wishlistRowItem tags, this will indicate each game that is in the list 
+            // //*[@id]
+            //var x = hdoc.DocumentNode.SelectNodes("//*[@class='wishlistRow ']");
+
+            foreach (HtmlNode wishlistRow in hdoc.DocumentNode.SelectNodes("//div[@class='wishlistRow ']"))
             {
+                gameID = wishlistRow.Id.Split('_')[1]; //<div class="wishlistRow " id="game_252250">
+
                 // Note: the web request (thru WebClient) seems to be slightly different than if you d/l through a browser...not sure why
                 HtmlDocument innerRow = new HtmlDocument();
                 HtmlDocument priceData = new HtmlDocument();
                 bool onSale = false;
-                innerRow.LoadHtml(wishlistRowItem.InnerHtml);
+                innerRow.LoadHtml(wishlistRow.InnerHtml);
 
                 // Fill data
                 // <h4 class="ellipsis">Big Pharma</h4>
@@ -78,14 +84,127 @@ namespace WishlistCompare
                 }
 
                 // Get lowest price data
-                lowestRegPrice = "$0.00"; // dummy data for now
-                lowestSalePrice = "$0.00";
+                string[] prices = GetLowestPrices(gameID).Split(':');
+                lowestRegPrice = prices[0];
+                lowestSalePrice = prices[1];
 
                 // Put the data into the collection
                 rawData.Add( String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}", gameName, gameRank, originalPrice, salePrice, salePercent, lowestRegPrice, lowestSalePrice) );
             }
 
             return rawData;
+        }
+        public string GetLowestPrices(string steamGameId)
+        {
+            WebClient client = new WebClient();
+            string steampricesUrl = @"https://www.steamprices.com/us/app/";
+            string pricesHtml = ""; string lowRegPrice = "", lowSalePrice = "";
+            steampricesUrl = steampricesUrl + steamGameId;
+
+            // Pull down the raw HTML as a string
+            try
+            {
+                pricesHtml = client.DownloadString(steampricesUrl);
+            }
+            catch (WebException wex)
+            {
+                // If there's a 404 error, the issue might be DLC (which is a different link)
+                if (wex.Message.Contains("404"))
+                {
+                    steampricesUrl = "https://www.steamprices.com/us/dlc/" + steamGameId;
+                    try
+                    {
+                        pricesHtml = client.DownloadString(steampricesUrl);
+                    }
+                    catch (WebException wex2)
+                    {
+                        Console.WriteLine("Exception: {0}", wex2.Message); //Replace with Logger
+                        // return with a zero value
+                        return "0:0";
+                    }
+                }
+            }
+            
+            HtmlDocument lowestPriceData = new HtmlDocument();
+            lowestPriceData.LoadHtml(pricesHtml);
+
+            // Timeout delay because if you hit the site too hard it cries
+            if (pricesHtml.Contains("Error 429 - Too Many Requests"))
+            {
+                System.Threading.Thread.Sleep((10 * 1000));
+                // Pull down the raw HTML as a string
+                try
+                {
+                    pricesHtml = client.DownloadString(steampricesUrl);
+                }
+                catch (WebException wex)
+                {
+                    // If there's a 404 error, the issue might be DLC (which is a different link)
+                    if (wex.Message.Contains("404"))
+                    {
+                        steampricesUrl = "https://www.steamprices.com/us/dlc/" + steamGameId;
+                        try
+                        {
+                            pricesHtml = client.DownloadString(steampricesUrl);
+                        }
+                        catch (WebException wex2)
+                        {
+                            Console.WriteLine("Exception: {0}", wex2.Message); //Replace with Logger
+                            // return with a zero value
+                            return "0:0";
+                        }
+                    }
+                }
+                lowestPriceData.LoadHtml(pricesHtml);
+            }
+
+            // Get the Price History data
+            //    //div[@id='history']
+            string priceHistory = lowestPriceData.DocumentNode.SelectSingleNode("//div[@id='history']").InnerHtml.ToString();
+            // For games that aren't released yet...
+            if (priceHistory.Contains("No price history"))
+                return "0:0";
+
+            HtmlDocument lowestPrices = new HtmlDocument();
+            lowestPrices.LoadHtml(priceHistory);
+
+            // Gets us the single or double pair of prices
+            //    //p[@class='nowrap']
+            HtmlNodeCollection priceData = lowestPrices.DocumentNode.SelectNodes("//p[@class='nowrap']");
+
+            foreach (HtmlNode price in priceData)
+            {
+                HtmlNodeCollection spans = price.ChildNodes;
+                bool bRegFound = false;
+                bool bSaleFound = false;
+                foreach (HtmlNode span in spans)
+                {
+                    switch (span.InnerText)
+                    {
+                        case "Lowest regular price:":
+                            bRegFound = true;
+                            break;
+                        case "Lowest discount price:":
+                            bSaleFound = true;
+                            break;
+                    }
+                    
+                    if ((span.GetAttributeValue("class","") == "price value"))
+                    {
+                        if (bRegFound) { lowRegPrice = span.InnerText; bRegFound = false; }
+                        if (bSaleFound) { lowSalePrice = span.InnerText; bSaleFound = false; }
+                    }
+
+                }
+            }
+
+            lowRegPrice = lowRegPrice.Substring(5, lowRegPrice.Length - 5);
+            if (!(String.IsNullOrEmpty(lowSalePrice)))
+                lowSalePrice = lowSalePrice.Substring(5, lowSalePrice.Length - 5);
+            else
+                lowSalePrice = "0";
+
+            return lowRegPrice + ":" + lowSalePrice;
         }
     }
 }
